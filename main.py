da5d09f0e0c7dbde67c90a018326a3724fc52338
Mihail_MSI <m89531276499@gmail.com>
2022-12-16 10:24:58 +0300

    Class group

--- main.py
+++ main.py
@@ -7,6 +7,49 @@ class Group:
         self.name = name
         self.number = number
         self._students = []
+    def __str__(self):
+        text = f'''
+        {self.number}{self.name} класс.
+        {len(self._students)} чел.
+        '''
+        return text
+    def __len__(self):
+        '''
+        len(Group)
+        :return: длину нужного нам списка
+        '''
+        return len(self._students)
+    def __getitem__(self, n):
+        '''
+        Group[1] берём по индексу...
+        :param n: запрашиваем нужный нам индекс
+        :return: значение из списка
+        '''
+        return self._students[n - 1]
+    def __contains__(self, item):
+        '''
+        Проверяем есть ли студент в списке
+        :param item: Студент...
+        :return: Bool
+        '''
+        return item in self._students
+    def __eq__(self, other):
+        return len(self) == len(other)
+    def __ne__(self, other):
+        return len(self) != len(other)
+    def __lt__(self, other):
+        return len(self) < len(other)
+    def __le__(self, other):
+        return len(self) <= len(other)
+    def __gt__(self, other):
+        return len(self) > len(other)
+    def __ge__(self, other):
+        return len(self) >= len(other)
+    def __add__(self, other):
+        new_stud = self._students + other._students
+        class_1B = Group(1, 'Б')
+        return self._students + other._students
+
 
     def add(self, student):
         '''
@@ -16,50 +59,15 @@ class Group:
         '''
         for i in student:
             self._students.append(i)
-
-    def __str__(self):
-        text =f'''
-        {self.number}{self.name} класс.
-        {len(self._students)} чел.
-        '''
-        return text
-
-    def __len__(self):
-        '''
-        len(Group)
-        :return: длину нужного нам списка
-        '''
-        return len(self._students)
-
-    def __getitem__(self, n):
-        '''
-        Group[1] берём по индексу...
-        :param n: запрашиваем нужный нам индекс
-        :return: значение из списка
-        '''
-        return self._students[n-1]
-
-    def __eq__(self, other):
-        '''
-        Сравнение классов
-        :param other: ==
-        :return:
-        '''
-        return len(self) == len(other)
-
-    def __contains__(self, item):
-        '''
-        Проверяем есть ли студент в списке
-        :param item: Студент...
-        :return: Bool
-        '''
-        return item in self._students
+    def remove(self, student):
+        return self._students.remove(student)
+    def show(self):
+        print(self._students)
 
 
-if __name__=='__main__':
-
+if __name__ == '__main__':
     lst1 = ['Jessica', 'Leo', 'Mike', 'Sofi']
-    lst2 = ['Maria', 'Nina', 'Jeck']
+    lst2 = ['Maria', 'Nina', 'Jack']
 
     class_1A = Group(1, 'A')
     class_1E = Group(1, 'E')
@@ -67,5 +75,7 @@ if __name__=='__main__':
     class_1A.add(lst1)
     class_1E.add(lst2)
 
+    print(len(class_1E), len(class_1A))
+    class_1A += class_1E
+    print(class_1A)
 
-    print('есть' if class_1E[1] in class_1A else 'нет')
